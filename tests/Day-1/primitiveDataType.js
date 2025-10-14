ed21d1f9b1622ca783c9a6779dad5edaceede1d5
Dhinakaran S <qa.dhinakaran@gmail.com>
2025-10-15 00:20:25 +0530

    Day 1 assignment

---
+++ tests/Day-1/primitiveDataType.js
@@ -0,0 +1,11 @@
+let userName = "Dhinakaran"
+let organization = "Testleaf"
+let contactNumber = 123456789
+let knowsAutomation = true
+let usesPlaywright 
+
+console.log("Username is : "+ userName +" and it's type is : "+ typeof userName);
+console.log("Organization name is : "+ organization +" and it's type is : "+ typeof organization);
+console.log("Contact number is : "+ contactNumber +" and it's type is : "+ typeof contactNumber);
+console.log("Automation knowledge is : "+ knowsAutomation +" and it's type is : "+ typeof knowsAutomation);
+console.log("Playwright exp is : "+ usesPlaywright +" and it's type is : "+ typeof usesPlaywright);
