76d3b074f45af66aa50ee80c71e4c45a9ba7fbed
Dhinakaran S <qa.dhinakaran@gmail.com>
2025-10-15 00:27:03 +0530

    Updated Project Files

--- tests/Day-1/primitiveDataType.js
+++ tests/Day-1/primitiveDataType.js
@@ -3,7 +3,6 @@ let organization = "Testleaf"
 let contactNumber = 123456789
 let knowsAutomation = true
 let usesPlaywright 
-
 console.log("Username is : "+ userName +" and it's type is : "+ typeof userName);
 console.log("Organization name is : "+ organization +" and it's type is : "+ typeof organization);
 console.log("Contact number is : "+ contactNumber +" and it's type is : "+ typeof contactNumber);
